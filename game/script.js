7036973fdb1d5aaef229fe367e5da3bcf2173604
SijmenL <lokerssijmen@gmail.com>
2024-04-04 13:01:20 +0200

    Game and training data

--- game/script.js
+++ game/script.js
@@ -1,11 +1,58 @@
-import {HandLandmarker, FilesetResolver} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
+import {FilesetResolver, HandLandmarker} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
 
-const demosSection = document.getElementById("demos");
+document.addEventListener('DOMContentLoaded', init);
+
 let handLandmarker = undefined;
 let runningMode = "IMAGE";
 let enableWebcamButton;
 let webcamRunning = false;
 
+let canvasWidth;
+let canvasHeight;
+
+let lastVideoTime = -1;
+let results = undefined;
+let pose;
+let sortedResults;
+
+let lastCircleSpawnTime = performance.now();
+const circleSpawnInterval = 1000;
+
+let networkPredictions = document.getElementById('predictions')
+
+let video = document.getElementById("webcam");
+let canvasElement = document.getElementById("output_canvas");
+let canvasCtx = canvasElement.getContext("2d");
+
+const bubbles = [];
+
+const nn = ml5.neuralNetwork({task: 'classification', debug: true})
+const modelDetails = {
+    model: 'model/model.json',
+    metadata: 'model/model_meta.json',
+    weights: 'model/model.weights.bin'
+}
+
+function init() {
+    createHandLandmarker();
+
+    const hasGetUserMedia = () => {
+        let _a;
+        return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia);
+    };
+
+    if (hasGetUserMedia()) {
+        enableWebcamButton = document.getElementById("webcamButton");
+        enableWebcamButton.addEventListener("click", enableCam);
+    } else {
+        console.warn("getUserMedia() is not supported by your browser");
+    }
+
+    nn.load(modelDetails, () => console.log("het model is geladen!"))
+
+    setCanvasSize();
+    window.addEventListener('resize', setCanvasSize);
+}
 
 const createHandLandmarker = async () => {
     const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
@@ -15,31 +62,11 @@ const createHandLandmarker = async () => {
             delegate: "GPU"
         },
         runningMode: runningMode,
-        numHands: 5
+        numHands: 1
     });
-    demosSection.classList.remove("invisible");
 };
-createHandLandmarker();
 
 
-const video = document.getElementById("webcam");
-const canvasElement = document.getElementById("output_canvas");
-const canvasCtx = canvasElement.getContext("2d");
-// Check if webcam access is supported.
-const hasGetUserMedia = () => {
-    let _a;
-    return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia);
-};
-// If webcam supported, add event listener to button for when user
-// wants to activate it.
-if (hasGetUserMedia()) {
-    enableWebcamButton = document.getElementById("webcamButton");
-    enableWebcamButton.addEventListener("click", enableCam);
-} else {
-    console.warn("getUserMedia() is not supported by your browser");
-}
-
-// Enable the live webcam view and start detection.
 function enableCam(event) {
     if (!handLandmarker) {
         console.log("Wait! objectDetector not loaded yet.");
@@ -49,54 +76,175 @@ function enableCam(event) {
         webcamRunning = false;
         enableWebcamButton.innerText = "Start Scannen";
     } else {
+
+        if (bubbles.length < 1) {
+            for (let i = 0; i < 25; i++) {
+                createCircle();
+            }
+        }
         webcamRunning = true;
         enableWebcamButton.innerText = "Stop Scannen";
     }
-    // getUsermedia parameters.
+
     const constraints = {
         video: true
     };
-    // Activate the webcam stream.
+
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
         video.srcObject = stream;
         video.addEventListener("loadeddata", predictWebcam);
     });
 }
 
-let lastVideoTime = -1;
-let results = undefined;
-console.log(video);
+
+function setCanvasSize() {
+    canvasElement.style.width = window.innerWidth + 'px';
+    canvasElement.style.height = window.innerHeight + 'px';
+    canvasElement.width = window.innerWidth;
+    canvasElement.height = window.innerHeight;
+
+    // Update canvas width and height variables
+    canvasWidth = canvasElement.width;
+    canvasHeight = canvasElement.height;
+}
 
 async function predictWebcam() {
-    canvasElement.style.width = video.videoWidth;
-    ;
-    canvasElement.style.height = video.videoHeight;
-    canvasElement.width = video.videoWidth;
-    canvasElement.height = video.videoHeight;
-    // Now let's start detecting the stream.
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
     if (runningMode === "IMAGE") {
         runningMode = "VIDEO";
         await handLandmarker.setOptions({runningMode: "VIDEO"});
     }
+
     let startTimeMs = performance.now();
     if (lastVideoTime !== video.currentTime) {
         lastVideoTime = video.currentTime;
         results = handLandmarker.detectForVideo(video, startTimeMs);
     }
+
     canvasCtx.save();
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
     if (results.landmarks) {
         for (const landmarks of results.landmarks) {
+            pose = landmarks.flatMap(coord => [coord.x, coord.y, coord.z]);
+
             drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
                 color: "#01ffff",
                 lineWidth: 1
             });
+
             drawLandmarks(canvasCtx, landmarks, {color: "#00c5ff", lineWidth: 1});
+
+            if (pose !== undefined) {
+                canvasCtx.beginPath();
+                canvasCtx.arc(pose[24] * canvasWidth, pose[25] * canvasHeight, 5, 0, Math.PI * 2);
+                canvasCtx.fillStyle = 'blue';
+                canvasCtx.fill();
+                canvasCtx.closePath();
+            }
+
+            const currentTime = performance.now();
+            if (currentTime - lastCircleSpawnTime >= circleSpawnInterval) {
+                createCircle();
+                lastCircleSpawnTime = currentTime;
+            }
+
+            bubbles.forEach((circle, index) => {
+                drawCircle(circle);
+
+                // Update circle position
+                circle.x += circle.dx;
+                circle.y += circle.dy;
+
+                // Check for collision with canvasElement edges
+                if (circle.x + circle.radius > canvasElement.width || circle.x - circle.radius < 0) {
+                    circle.dx = -circle.dx; // Reverse horizontal velocity
+                }
+                if (circle.y + circle.radius > canvasElement.height || circle.y - circle.radius < 0) {
+                    circle.dy = -circle.dy; // Reverse vertical velocity
+                }
+
+                const distanceToPose = Math.sqrt((circle.x - pose[24] * canvasWidth) ** 2 + (circle.y - pose[25] * canvasHeight) ** 2);
+                if (distanceToPose <= circle.radius && sortedResults[0].label === 'pointing') {
+                    if (circle.color !== '#2596be') {
+                        console.log("player died");
+                        stopGame();
+                    } else {
+                        bubbles.splice(index, 1);
+                    }
+                }
+            });
+
+            let predictions = await nn.classify(pose);
+            sortedResults = predictions.sort((a, b) => b.confidence - a.confidence);
+
+            let fragment = document.createDocumentFragment();
+
+            sortedResults.forEach(prediction => {
+                let p = document.createElement('p');
+                p.innerText = `${prediction.label}: ${prediction.confidence}`;
+                fragment.appendChild(p);
+            });
+
+            networkPredictions.innerHTML = '';
+            networkPredictions.appendChild(fragment);
         }
     }
+
+    // Draw circles
+    bubbles.forEach(circle => {
+        drawCircle(circle);
+
+        // Update circle position
+        circle.x += circle.dx;
+        circle.y += circle.dy;
+
+        // Check for collision with canvasElement edges
+        if (circle.x + circle.radius > canvasElement.width || circle.x - circle.radius < 0) {
+            circle.dx = -circle.dx; // Reverse horizontal velocity
+        }
+        if (circle.y + circle.radius > canvasElement.height || circle.y - circle.radius < 0) {
+            circle.dy = -circle.dy; // Reverse vertical velocity
+        }
+    });
+
     canvasCtx.restore();
-    // Call this function again to keep predicting when the browser is ready.
+
+
     if (webcamRunning === true) {
         window.requestAnimationFrame(predictWebcam);
     }
+}
+
+function createCircle() {
+    // Create new circle
+    const radius = Math.random() * 20 + 10;
+    const x = Math.random() * (canvasElement.width - 2 * radius) + radius;
+    const y = Math.random() * (canvasElement.height - 2 * radius) + radius;
+    const dx = (Math.random() - 0.5) * 2;
+    const dy = (Math.random() - 0.5) * 2;
+
+    let circleColor;
+    if (Math.random() < 0.2) {
+        circleColor = '#be2525';
+    } else {
+        circleColor = '#2596be';
+    }
+    const color = circleColor
+
+    bubbles.push({ x, y, radius, dx, dy, color });
+}
+
+function drawCircle(circle) {
+    canvasCtx.beginPath();
+    canvasCtx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
+    canvasCtx.fillStyle = 'transparent';
+    canvasCtx.strokeStyle = circle.color;
+    canvasCtx.lineWidth = 3;
+    canvasCtx.stroke();
+    canvasCtx.fill();
+    canvasCtx.closePath();
+}
+
+function stopGame() {
+    webcamRunning = false;
 }
--- training/script.js
+++ training/script.js
@@ -1,11 +1,60 @@
 import {HandLandmarker, FilesetResolver} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
 
-const demosSection = document.getElementById("demos");
+document.addEventListener('DOMContentLoaded', init);
+
 let handLandmarker = undefined;
 let runningMode = "IMAGE";
 let enableWebcamButton;
 let webcamRunning = false;
+let data = [];
 
+let importedTrainingData;
+
+let lastVideoTime = -1;
+let results = undefined;
+
+let networkPredictions = document.getElementById('predictions')
+
+let trainingStarted = false;
+let label = '';
+let pose;
+let modelReady = false;
+
+let labelInput = document.getElementById('label')
+let trainingButton = document.getElementById('trainingButton')
+let predictPose = document.getElementById('predictPose')
+let inportButton = document.getElementById('importData')
+let saveModel = document.getElementById('saveModel')
+let video = document.getElementById("webcam");
+let canvasElement = document.getElementById("output_canvas");
+let canvasCtx = canvasElement.getContext("2d");
+
+const nn = ml5.neuralNetwork({task: 'classification', debug: true})
+
+function init() {
+    createHandLandmarker();
+
+    const hasGetUserMedia = () => {
+        let _a;
+        return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia);
+    };
+
+    if (hasGetUserMedia()) {
+        enableWebcamButton = document.getElementById("webcamButton");
+        enableWebcamButton.addEventListener("click", enableCam);
+    } else {
+        console.warn("getUserMedia() is not supported by your browser");
+    }
+
+
+    trainingButton.addEventListener('click', startTraining);
+    inportButton.addEventListener('click', fetchData);
+    predictPose.addEventListener('click', makePrediction);
+
+    saveModel.addEventListener('click', function () {
+        nn.save("model", () => console.log("model was saved!"))
+    });
+}
 
 const createHandLandmarker = async () => {
     const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
@@ -15,31 +64,11 @@ const createHandLandmarker = async () => {
             delegate: "GPU"
         },
         runningMode: runningMode,
-        numHands: 5
+        numHands: 1
     });
-    demosSection.classList.remove("invisible");
 };
-createHandLandmarker();
 
 
-const video = document.getElementById("webcam");
-const canvasElement = document.getElementById("output_canvas");
-const canvasCtx = canvasElement.getContext("2d");
-// Check if webcam access is supported.
-const hasGetUserMedia = () => {
-    let _a;
-    return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia);
-};
-// If webcam supported, add event listener to button for when user
-// wants to activate it.
-if (hasGetUserMedia()) {
-    enableWebcamButton = document.getElementById("webcamButton");
-    enableWebcamButton.addEventListener("click", enableCam);
-} else {
-    console.warn("getUserMedia() is not supported by your browser");
-}
-
-// Enable the live webcam view and start detection.
 function enableCam(event) {
     if (!handLandmarker) {
         console.log("Wait! objectDetector not loaded yet.");
@@ -52,46 +81,78 @@ function enableCam(event) {
         webcamRunning = true;
         enableWebcamButton.innerText = "Stop Scannen";
     }
-    // getUsermedia parameters.
+
     const constraints = {
         video: true
     };
-    // Activate the webcam stream.
+
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
         video.srcObject = stream;
         video.addEventListener("loadeddata", predictWebcam);
     });
 }
 
-let lastVideoTime = -1;
-let results = undefined;
-console.log(video);
 
 async function predictWebcam() {
     canvasElement.style.width = video.videoWidth;
-    ;
     canvasElement.style.height = video.videoHeight;
     canvasElement.width = video.videoWidth;
     canvasElement.height = video.videoHeight;
-    // Now let's start detecting the stream.
+
     if (runningMode === "IMAGE") {
         runningMode = "VIDEO";
         await handLandmarker.setOptions({runningMode: "VIDEO"});
     }
+
     let startTimeMs = performance.now();
     if (lastVideoTime !== video.currentTime) {
         lastVideoTime = video.currentTime;
         results = handLandmarker.detectForVideo(video, startTimeMs);
     }
+
     canvasCtx.save();
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
     if (results.landmarks) {
+
         for (const landmarks of results.landmarks) {
+
+            const coordinatesArray = landmarks.flatMap(coord => [coord.x, coord.y, coord.z]);
+            pose = coordinatesArray;
+
+            if (trainingStarted) {
+                const trainingObject = ({
+                    pose: coordinatesArray,
+                    label: labelInput.value
+                });
+
+                data.push(trainingObject);
+
+                localStorage.setItem('training', JSON.stringify(data));
+
+                console.log(trainingObject);
+            }
+
             drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
                 color: "#01ffff",
                 lineWidth: 1
             });
             drawLandmarks(canvasCtx, landmarks, {color: "#00c5ff", lineWidth: 1});
+
+            if (modelReady) {
+                let fragment = document.createDocumentFragment();
+
+                let predictions = await nn.classify(pose);
+                let sortedResults = predictions.sort((a, b) => b.confidence - a.confidence);
+
+                sortedResults.forEach(prediction => {
+                    let p = document.createElement('p');
+                    p.innerText = `${prediction.label}: ${prediction.confidence}`;
+                    fragment.appendChild(p);
+                });
+
+                networkPredictions.innerHTML = '';
+                networkPredictions.appendChild(fragment);
+            }
         }
     }
     canvasCtx.restore();
@@ -99,4 +160,64 @@ async function predictWebcam() {
     if (webcamRunning === true) {
         window.requestAnimationFrame(predictWebcam);
     }
+}
+
+function startTraining() {
+    console.log(trainingStarted)
+
+    if (!trainingStarted) {
+        if (labelInput.value !== '') {
+            label = labelInput.value
+            trainingButton.innerHTML = 'Stop Training'
+            trainingStarted = true;
+        } else {
+            trainingButton.innerHTML = 'Start Training'
+            trainingStarted = false;
+        }
+    } else {
+        trainingStarted = false;
+        trainingButton.innerHTML = 'Start Training'
+    }
+}
+
+async function fetchData() {
+    try {
+        const response = await fetch('trainingData.json'); // Fetch pointing.json
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+        importedTrainingData = await response.json();
+
+        importedTrainingData = importedTrainingData.toSorted(() => (Math.random() - 0.5))
+
+        console.log(importedTrainingData);
+
+        importedTrainingData.forEach(entry => {
+            const coordinates = entry.pose;
+            const label = entry.label;
+            nn.addData(coordinates, {label});
+        });
+
+        startTrainingNN()
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function startTrainingNN() {
+    nn.normalizeData()
+
+    nn.train({epochs: 20}, () => finishedTraining())
+}
+
+async function finishedTraining() {
+    inportButton.innerHTML = 'Done!';
+    console.log("Finished training!")
+    modelReady = true;
+}
+
+async function makePrediction() {
+    const results = await nn.classify(pose)
+    console.log(results)
 }
